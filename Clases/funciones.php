8e309eee97b5cb1b88824068c72eba62cd682561
dcantos <titojo12@gmail.com>
2014-03-20 09:56:48 -0500

    añadieron nuevas funcionalidades

--- Clases/funciones.php
+++ Clases/funciones.php
@@ -45,12 +45,6 @@
 						<textarea placeholder="<?php esc_attr_e( "Remember: keep it short! To insert an image, drag and drop it here.", 'liveblog' ); ?>" class="liveblog-form-entry" name="liveblog-form-entry" cols="50" rows="5">{{content}}</textarea>
 					<div class="liveblog-submit-wrapper">
 						
-						<span class="liveblog-submit-spinner"></span>
-						<input type="button" class="liveblog-form-entry-submit button" value="{{submit_label}}" />
-						<a href="#" class="cancel">Cancel</a>
-					</div>
-				</div>
-			<div class="liveblog-preview">
 			</div>
 	</fieldset>
 </script>
